02b965ea0bc1e705131dc09d7550b657aa96c4cd
Purwa Sadr <purwasadr@gmail.com>
2021-04-22 08:34:00 +0700

    Setup Adapter for cash in DetailActivity

---
+++ app/src/main/java/com/alurwa/moviecatalogue/core/common/CastAdapter.kt
@@ -0,0 +1,23 @@
+package com.alurwa.moviecatalogue.core.common
+
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.alurwa.moviecatalogue.databinding.RcvItemCastBinding
+
+class CastAdapter : RecyclerView.Adapter<CastAdapter.ViewHolder>() {
+    inner class ViewHolder(binding: RcvItemCastBinding) : RecyclerView.ViewHolder(binding.root) {
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+}
