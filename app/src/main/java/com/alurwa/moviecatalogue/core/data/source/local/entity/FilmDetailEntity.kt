14cc2f73b6752b62eb5be9315718f3a7fcdbb8c7
Purwa Sadr <purwasadr@gmail.com>
2021-05-22 15:09:43 +0700

    Remove unused annotation

--- app/src/main/java/com/alurwa/moviecatalogue/core/data/source/local/entity/FilmDetailEntity.kt
+++ app/src/main/java/com/alurwa/moviecatalogue/core/data/source/local/entity/FilmDetailEntity.kt
@@ -1,6 +1,5 @@
 package com.alurwa.moviecatalogue.core.data.source.local.entity
 
-import androidx.annotation.NonNull
 import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
@@ -10,40 +9,38 @@ import com.alurwa.moviecatalogue.core.model.Genre
 @Entity(tableName = "film_detail")
 data class FilmDetailEntity(
 
-        @PrimaryKey
-        @NonNull
-        @ColumnInfo(name = "id")
-        val id: Int,
+    @PrimaryKey
+    val id: Int,
 
-        @ColumnInfo(name = "title")
-        val title: String,
+    @ColumnInfo(name = "title")
+    val title: String,
 
-        val overview: String?,
+    val overview: String?,
 
-        @ColumnInfo(name = "poster_path")
-        val posterPath: String?,
+    @ColumnInfo(name = "poster_path")
+    val posterPath: String?,
 
-        @ColumnInfo(name = "backdrop_path")
-        val backdropPath: String?,
+    @ColumnInfo(name = "backdrop_path")
+    val backdropPath: String?,
 
-        @ColumnInfo(name = "release_date")
-        val releaseDate: String,
+    @ColumnInfo(name = "release_date")
+    val releaseDate: String,
 
-        val revenue: Int,
+    val revenue: Int,
 
-        val budget: Int,
+    val budget: Int,
 
-        @ColumnInfo(name = "original_language")
-        val originalLanguage: String,
+    @ColumnInfo(name = "original_language")
+    val originalLanguage: String,
 
-        val runtime: Int?,
+    val runtime: Int?,
 
-        val status: String,
+    val status: String,
 
-        @ColumnInfo(name = "vote_average")
-        val voteAverage: Number,
+    @ColumnInfo(name = "vote_average")
+    val voteAverage: Number,
 
-        val genres: List<Genre>,
+    val genres: List<Genre>,
 
-        val cast: List<Cast>
+    val cast: List<Cast>
 )
