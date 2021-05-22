2ff98928d878ff754abf1be980946f13f5c28284
Purwa Sadr <purwasadr@gmail.com>
2021-05-22 15:20:30 +0700

    Try to fix item margin in end

--- app/src/main/java/com/alurwa/moviecatalogue/core/common/CastAdapter.kt
+++ app/src/main/java/com/alurwa/moviecatalogue/core/common/CastAdapter.kt
@@ -2,27 +2,26 @@ package com.alurwa.moviecatalogue.core.common
 
 import android.content.res.Resources
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
-import androidx.core.view.updateLayoutParams
-import androidx.core.view.updatePaddingRelative
 import androidx.recyclerview.widget.RecyclerView
 import com.alurwa.moviecatalogue.core.model.Cast
 import com.alurwa.moviecatalogue.databinding.RcvItemCastBinding
-import com.bumptech.glide.load.engine.Resource
 
-class CastAdapter(private val castList: List<Cast>?) : RecyclerView.Adapter<CastAdapter.ViewHolder>() {
+class CastAdapter(
+    private val castList: List<Cast>?
+) : RecyclerView.Adapter<CastAdapter.ViewHolder>() {
 
     val Int.px: Int
-    get() = (this * Resources.getSystem().displayMetrics.density).toInt()
+        get() = (this * Resources.getSystem().displayMetrics.density).toInt()
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder =
-            ViewHolder(
-                    RcvItemCastBinding.inflate(
-                            LayoutInflater.from(parent.context),
-                            parent,
-                            false)
+        ViewHolder(
+            RcvItemCastBinding.inflate(
+                LayoutInflater.from(parent.context),
+                parent,
+                false
             )
+        )
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         holder.bind(position)
@@ -31,7 +30,7 @@ class CastAdapter(private val castList: List<Cast>?) : RecyclerView.Adapter<Cast
     override fun getItemCount(): Int = castList?.size ?: 0
 
     inner class ViewHolder(
-            private val binding: RcvItemCastBinding
+        private val binding: RcvItemCastBinding
     ) : RecyclerView.ViewHolder(binding.root) {
         fun bind(position: Int) {
             val cast = castList?.get(position)
@@ -41,7 +40,7 @@ class CastAdapter(private val castList: List<Cast>?) : RecyclerView.Adapter<Cast
                 //  val cardCastParams = binding.cardCast.layoutParams as ViewGroup.MarginLayoutParams
                 //   cardCastParams.marginEnd = 16.px
                 binding.isLastItem = true
-            }  else {
+            } else {
                 binding.isLastItem = false
             }
 
