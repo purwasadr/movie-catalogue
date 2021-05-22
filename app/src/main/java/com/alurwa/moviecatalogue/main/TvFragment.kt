fff37b34805e878b7646b21e8907dece114b6672
Purwa Sadr <purwasadr@gmail.com>
2021-05-22 15:23:58 +0700

    Remove companion object

--- app/src/main/java/com/alurwa/moviecatalogue/main/TvFragment.kt
+++ app/src/main/java/com/alurwa/moviecatalogue/main/TvFragment.kt
@@ -9,11 +9,6 @@ import kotlinx.coroutines.launch
 
 
 class TvFragment : MovieFragmentAbstract() {
-
-    companion object {
-        const val TAG = "TvFragment"
-    }
-
     override fun navigateToDetail(extraId: Int) {
         Intent(requireContext(), TvDetailActivity::class.java)
             .putExtra(Constants.EXTRA_ID, extraId)
