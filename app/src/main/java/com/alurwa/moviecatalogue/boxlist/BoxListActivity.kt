e014b12ecba87469baf35afb54a646a82064a581
Purwa Sadr <purwasadr@gmail.com>
2021-05-30 22:27:58 +0700

    Configure list

--- app/src/main/java/com/alurwa/moviecatalogue/boxlist/BoxListActivity.kt
+++ app/src/main/java/com/alurwa/moviecatalogue/boxlist/BoxListActivity.kt
@@ -1,20 +1,25 @@
 package com.alurwa.moviecatalogue.boxlist
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.lifecycleScope
+import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.alurwa.moviecatalogue.core.adapter.BoxMovieAdapter
+import com.alurwa.moviecatalogue.core.adapter.MovieLoadStateAdapter
+import com.alurwa.moviecatalogue.core.common.FilmOrTv
 import com.alurwa.moviecatalogue.core.common.SpacingDecoration
 import com.alurwa.moviecatalogue.databinding.ActivityBoxListBinding
-import com.google.android.flexbox.AlignItems
-import com.google.android.flexbox.FlexDirection
-import com.google.android.flexbox.FlexboxLayoutManager
-import com.google.android.flexbox.JustifyContent
+import com.alurwa.moviecatalogue.filmdetail.FilmDetailActivity
+import com.alurwa.moviecatalogue.tvdetail.TvDetailActivity
+import com.alurwa.moviecatalogue.utils.CommonUtil
+import com.alurwa.moviecatalogue.utils.Constants
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.flow.collectLatest
 import kotlinx.coroutines.launch
+import java.util.*
 
 @AndroidEntryPoint
 class BoxListActivity : AppCompatActivity() {
@@ -24,7 +29,7 @@ class BoxListActivity : AppCompatActivity() {
 
     private val adapter: BoxMovieAdapter by lazy {
         BoxMovieAdapter() {
-
+            navigateToDetail(it)
         }
     }
 
@@ -34,25 +39,80 @@ class BoxListActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
 
+        setupToolbar()
+
         setupRecyclerView()
 
         getMovies()
     }
 
     private fun setupRecyclerView() {
-        with(binding) {
-            val flexboxLayoutManager = FlexboxLayoutManager(applicationContext)
+        // set Column width with total view with dp and margin which you want
+        val noOfColumn = CommonUtil.calculateNoOfColumns(
+            applicationContext,
+            132F
+        )
 
-            flexboxLayoutManager.flexDirection = FlexDirection.ROW
-            flexboxLayoutManager.alignItems = AlignItems.BASELINE
-            flexboxLayoutManager.justifyContent = JustifyContent.SPACE_AROUND
+        val gridLayoutManager = GridLayoutManager(
+            applicationContext,
+            noOfColumn
+        )
 
-            list.setHasFixedSize(true)
-            list.addItemDecoration(SpacingDecoration(16, RecyclerView.VERTICAL))
-            list.layoutManager = flexboxLayoutManager
-            list.adapter = adapter
-            list.itemAnimator = null
+        gridLayoutManager.spanSizeLookup = object : GridLayoutManager.SpanSizeLookup() {
+            override fun getSpanSize(position: Int): Int {
+                val viewType = adapter.getItemViewType(position)
+                return if (viewType == BoxMovieAdapter.VIEW_TYPE_MOVIE) 1 else noOfColumn
+
+            }
         }
+
+        with(binding) {
+            list.setHasFixedSize(true)
+            list.addItemDecoration(
+                SpacingDecoration(
+                    16,
+                    16,
+                    RecyclerView.VERTICAL
+                )
+            )
+            list.layoutManager = gridLayoutManager
+            list.adapter = adapter.withLoadStateHeaderAndFooter(
+                MovieLoadStateAdapter(),
+                MovieLoadStateAdapter() {
+                    adapter.retry()
+                }
+            )
+        }
+    }
+
+    private fun setupToolbar() {
+        supportActionBar?.apply {
+            title = viewModel.movieSort
+                .lowercase()
+                .replace('_', ' ', true)
+                .replaceFirstChar { it.titlecase(Locale.getDefault()) }
+
+            setDisplayHomeAsUpEnabled(true)
+        }
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        onBackPressed()
+        return true
+    }
+
+    private fun navigateToDetail(extraId: Int) {
+        Intent().putExtra(Constants.EXTRA_ID, extraId)
+            .also {
+                if (viewModel.filmOrTv == FilmOrTv.FILM.code) {
+                    it.setClass(this, FilmDetailActivity::class.java)
+
+                } else {
+                    it.setClass(this, TvDetailActivity::class.java)
+                }
+                startActivity(it)
+            }
+
     }
 
     private fun getMovies() {
@@ -61,5 +121,11 @@ class BoxListActivity : AppCompatActivity() {
                 adapter.submitData(it)
             }
         }
+    }
+
+    companion object {
+        const val EXTRA_FILM_OR_MOVIE = "extra_film_or_movie"
+
+        const val EXTRA_MOVIE_SORT = "extra_movie_sort"
     }
 }
--- app/src/main/java/com/alurwa/moviecatalogue/boxlist/BoxListViewModel.kt
+++ app/src/main/java/com/alurwa/moviecatalogue/boxlist/BoxListViewModel.kt
@@ -2,6 +2,9 @@ package com.alurwa.moviecatalogue.boxlist
 
 import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import androidx.paging.cachedIn
+import com.alurwa.moviecatalogue.core.common.FilmOrTv
 import com.alurwa.moviecatalogue.core.data.IMovieCatalogueRepository
 import com.alurwa.moviecatalogue.main.MovieSortEnum
 import dagger.hilt.android.lifecycle.HiltViewModel
@@ -16,6 +19,18 @@ class BoxListViewModel @Inject constructor(
     private val stateHandle: SavedStateHandle,
     private val repository: IMovieCatalogueRepository
 ) : ViewModel() {
-    val movies = repository.getFilms(MovieSortEnum.POPULAR)
+    val movies = if (filmOrTv == FilmOrTv.FILM.code) {
+        repository.getFilms(MovieSortEnum.valueOf(movieSort)).cachedIn(viewModelScope)
+    } else if (filmOrTv == FilmOrTv.TV.code) {
+        repository.getTvList(MovieSortEnum.valueOf(movieSort)).cachedIn(viewModelScope)
+    } else {
+        throw IllegalArgumentException("Intent not match any")
+    }
+
+    val filmOrTv
+    get() = stateHandle.get<Int>(BoxListActivity.EXTRA_FILM_OR_MOVIE)!!
+
+    val movieSort
+    get() = stateHandle.get<String>(BoxListActivity.EXTRA_MOVIE_SORT)!!
 
 }
