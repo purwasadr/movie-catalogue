0f7e8a7fdaf9a7248aad968104faa629bd147ff0
Purwa Sadr <purwasadr@gmail.com>
2021-06-16 19:41:44 +0700

    Remove unused function

--- app/src/main/java/com/alurwa/moviecatalogue/utils/NumberFormatUtil.kt
+++ app/src/main/java/com/alurwa/moviecatalogue/utils/NumberFormatUtil.kt
@@ -28,18 +28,4 @@ import java.text.DecimalFormat
 
 object NumberFormatUtil {
     fun withComma(value: Int): String = DecimalFormat("#,###").format(value)
-
-    fun timeHoursAndMinute(time: Int): String {
-        var result = ""
-        val ba = (time / 60)
-        result = if (time % 60 == 0 && time >= 60) {
-            "$ba j "
-        } else if (time < 60) {
-            "$time m"
-        } else {
-            "$ba j " + (time % 60) + " m"
-        }
-
-        return result
-    }
 }
