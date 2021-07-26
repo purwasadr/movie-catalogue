b835ab87f9098e3cb10f20cd5798b3b4bd55486f
Purwa Sadr <purwasadr@gmail.com>
2021-07-26 20:39:04 +0700

    Use static method for simple

--- app/src/main/java/com/alurwa/moviecatalogue/core/data/source/remote/RemoteDataSource.kt
+++ app/src/main/java/com/alurwa/moviecatalogue/core/data/source/remote/RemoteDataSource.kt
@@ -24,10 +24,8 @@
 
 package com.alurwa.moviecatalogue.core.data.source.remote
 
-import com.alurwa.moviecatalogue.core.data.source.remote.network.ApiResponse
 import com.alurwa.moviecatalogue.core.data.source.remote.network.ApiService
-import com.alurwa.moviecatalogue.core.data.source.remote.response.FilmDetailResponse
-import com.alurwa.moviecatalogue.core.data.source.remote.response.TvDetailResponse
+import com.alurwa.moviecatalogue.utils.handleRequest
 import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.flow.flowOn
@@ -37,61 +35,24 @@ class RemoteDataSource @Inject constructor(
     private val apiService: ApiService,
     private val dispatchers: CoroutineDispatcher
 ) : IRemoteDataSource {
-   /* suspend fun getAllMovies(): Flow<ApiResponse<List<MovieResponse>>> {
-        return flow {
-            try {
-                val response = apiService.getAllMovies()
-                val dataArray = response.results
 
-                if (dataArray.isNotEmpty()) {
-                    emit(ApiResponse.Success(response.results))
-                } else {
-                    emit(ApiResponse.Empty)
-                }
-
-            } catch (e: Exception) {
-                emit(ApiResponse.Error(e.toString()))
-            }
-
-        }.flowOn(Dispatchers.IO)
-    }
-
-    suspend fun getMovieDiscover(): Flow<ApiResponse<List<MovieResponse>>> {
-        return flow {
-            try {
-                val response = apiService.getAllMovies()
-                val dataArray = response.results
-
-                if (dataArray.isNotEmpty()) {
-                    emit(ApiResponse.Success(response.results))
-                } else {
-                    emit(ApiResponse.Empty)
-                }
-
-            } catch (e: Exception) {
-                emit(ApiResponse.Error(e.toString()))
-            }
-
-        }.flowOn(Dispatchers.IO)
-    }
-
-    */
-
-    override fun getFilmDetail(id: Int) = flow<ApiResponse<FilmDetailResponse>> {
-        try {
-            val results = apiService.getFilmDetail(id)
-            emit(ApiResponse.Success(results))
-        } catch (e: Exception) {
-            emit(ApiResponse.Error(e.toString()))
-        }
+    override fun getFilmDetail(id: Int) = flow {
+        emit(handleRequest { apiService.getFilmDetail(id) })
+//        try {
+//            val results = apiService.getFilmDetail(id)
+//            emit(ApiResponse.Success(results))
+//        } catch (e: Exception) {
+//            emit(ApiResponse.Error(e.toString()))
+//        }
     }.flowOn(dispatchers)
 
-    override fun getTvDetail(id: Int) = flow<ApiResponse<TvDetailResponse>> {
-        try {
-            val results = apiService.getTvDetail(id)
-            emit(ApiResponse.Success(results))
-        } catch (e: Exception) {
-            emit(ApiResponse.Error(e.toString()))
-        }
+    override fun getTvDetail(id: Int) = flow {
+//        try {
+//            val results = apiService.getTvDetail(id)
+//            emit(ApiResponse.Success(results))
+//        } catch (e: Exception) {
+//            emit(ApiResponse.Error(e.toString()))
+//        }
+        emit(handleRequest { apiService.getTvDetail(id) })
     }.flowOn(dispatchers)
 }
